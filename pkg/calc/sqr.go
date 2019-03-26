bccb9215b2ad44045dd592d9e2857370f3b36d2d
Guus van Weelden <guus.vanweelden@moia.io>
2019-03-26 09:10:16 +0100

    add benchmarks

--- pkg/calc/sqr.go
+++ pkg/calc/sqr.go
@@ -1,6 +1,18 @@
 package calc
 
+import "math"
+
 // Sqr multiples number with itself
 func Sqr(a int) int {
 	return Mul(a, a)
+}
+
+// Sqr multiples number with itself
+func SqrNative(a int) int {
+	return a * a
+}
+
+// Sqr multiples number with itself
+func SqrMath(a int) int {
+	return int(math.Pow(float64(a), 2.0))
 }
---
+++ pkg/calc/sqr_test.go
@@ -0,0 +1,92 @@
+package calc
+
+import (
+	"fmt"
+	"testing"
+)
+
+type args struct {
+	a int
+}
+
+type testSet struct {
+	name string
+	args args
+	want int
+}
+
+var (
+	tests = []testSet{
+		{
+			name: "0 is 0",
+			args: args{
+				a: 0,
+			},
+			want: 0,
+		},
+		{
+			name: "2 is 4",
+			args: args{
+				a: 2,
+			},
+			want: 4,
+		},
+		{
+			name: "3 is 9",
+			args: args{
+				a: 3,
+			},
+			want: 9,
+		},
+	}
+)
+
+func TestSqr(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Sqr(tt.args.a); got != tt.want {
+				t.Errorf("Sqr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSqrNative(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SqrNative(tt.args.a); got != tt.want {
+				t.Errorf("SqrNative() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSqrMath(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SqrMath(tt.args.a); got != tt.want {
+				t.Errorf("SqrMath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func BenchmarkSqr(b *testing.B) {
+	sqrs := []struct {
+		name string
+		fun  func(a int) int
+	}{
+		{"slow", Sqr},
+		{"native", SqrNative},
+		{"math", SqrMath},
+	}
+	for _, sqr := range sqrs {
+		for k := 100; k <= 500; k = k + 100 {
+			b.Run(fmt.Sprintf("%s/%d", sqr.name, k), func(b *testing.B) {
+				for i := 0; i < b.N; i++ {
+					sqr.fun(k)
+				}
+			})
+		}
+	}
+}
